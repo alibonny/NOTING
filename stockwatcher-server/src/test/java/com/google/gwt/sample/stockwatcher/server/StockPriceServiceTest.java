1ddb21aa9fb2d723a0e12a85568cae86449af8a9
Marco Ferrati <marco.ferrati@gmail.com>
2022-11-23 11:24:05 +0100

    Add dependencies for JUinit5 and Surefire plugin

---
+++ stockwatcher-server/src/test/java/com/google/gwt/sample/stockwatcher/server/StockPriceServiceTest.java
@@ -0,0 +1,27 @@
+package com.google.gwt.sample.stockwatcher.server;
+
+import com.google.gwt.sample.stockwatcher.shared.StockPrice;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StockPriceServiceTest {
+
+    StockPriceServiceImpl service = new StockPriceServiceImpl();
+
+    @BeforeEach
+    public void cleanEnvironment() {
+
+    }
+
+    @DisplayName("Test testing is working")
+    @Test
+    public void testGetPrices() {
+        String[] symbols = new String[] {"FTSEMIB", "APL", "AMD", "INTC"};
+        StockPrice[] stockPrices = service.getPrices(symbols);
+
+        assertEquals(stockPrices.length, symbols.length);
+    }
+}
